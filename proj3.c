e1de3eeccd521f544153fbca128a88344a4aefc7
Luke <luke@localhost.localdomain>
2019-10-15 23:05:32 -0400

    fukin working now bois

--- proj3.c
+++ proj3.c
@@ -7,36 +7,84 @@
 #include <linux/kprobes.h>
 #include <linux/hashtable.h>
 #include <linux/sched.h>
-#define BITS 8
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Luke Sanyour");
 MODULE_DESCRIPTION("Project 3 Perftop");
 
-//static DEFINE_HASHTABLE(pid_hashtable, BITS);
-static int pid = 0;
-//return handler to increment count of times perftop has been called
-static int ret_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
-{
-	if(regs_return_value(regs))
-	{
-		struct task_struct *p = (struct task_struct *)regs_return_value(regs);
-		pid = p->pid;
+static DEFINE_HASHTABLE(pidHashtable, 16);
+struct hash_entry {
 
+	int pid_key;
+	int numSchedules;
+	struct hlist_node hash_list;
+};
+void addPid(int pid)
+{	
+	int bkt;
+	struct hash_entry *current_hash_entry;
+	struct hash_entry *he = kmalloc(sizeof(*he), GFP_ATOMIC);
+	if(!hash_empty(pidHashtable))
+	{
+		hash_for_each(pidHashtable, bkt, current_hash_entry, hash_list)
+		{
+			if(current_hash_entry->pid_key == pid)
+			{
+				current_hash_entry->numSchedules++;
+				//printk(KERN_INFO "NumSched: %d\n", current_hash_entry->numSchedules);
+				return;
+			}
+		}
+	}
+
+	if(he != NULL)
+	{
+		he->pid_key = pid;
+		he->numSchedules = 1;
+		hash_add(pidHashtable, &he->hash_list, pid);
+	}
+
+}
+void deleteHashTable(void)
+{
+	int bkt;
+	struct hash_entry *current_hash_entry;
+	hash_for_each(pidHashtable, bkt, current_hash_entry, hash_list)
+	{
+		hash_del(&current_hash_entry->hash_list);
+	}
+}
+static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
+{
+	//retrive current task, which is the second argument of pick_next_task_fair,
+	//stored in rsi register by standard x86 64-bit calling convention
+	if(regs->si)
+	{
+		struct task_struct *p = (struct task_struct *)regs->si;
+		if(p != NULL && p->pid)
+		{	
+			addPid(p->pid);
+		}
 	}
 	return 0;
 }
 //create proc file
 static int perftop_show(struct seq_file *m, void *v) {
-	seq_printf(m, "%d\n", pid);
+	int bkt;
+	struct hash_entry *current_hash_entry;	
+	hash_for_each(pidHashtable, bkt, current_hash_entry, hash_list)
+	{
+		seq_printf(m, "PID: %d      # Of Schedules: %d\n ", current_hash_entry->pid_key, current_hash_entry->numSchedules);
+	}
 	return 0;
+
 }
 
 static int perftop_open(struct inode *inode, struct  file *file) {
 	return single_open(file, perftop_show, NULL);
 }
 static struct kretprobe perftop_kretprobe = {
-	.handler		= ret_handler,
+	.entry_handler 		= entry_handler,
 	.kp.symbol_name = "pick_next_task_fair",
 	.maxactive = 1,
 
@@ -54,13 +102,15 @@ static const struct file_operations perftop_fops = {
 static int __init proj3_init(void)
 {
 	int ret;
-	proc_create("perftop", 0, NULL, &perftop_fops);
 	ret = register_kretprobe(&perftop_kretprobe);
+	proc_create("perftop", 0, NULL, &perftop_fops);
 	return 0;
 }
 //exit module
 static void __exit proj3_exit(void)
 {
+	//delete hash table
+	deleteHashTable();
 	unregister_kretprobe(&perftop_kretprobe);
 	remove_proc_entry("perftop", NULL);
 }
