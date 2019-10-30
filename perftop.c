ae679d35a7bcddbca077c779bc631634db2ee3a5
Luke <luke@localhost.localdomain>
2019-10-29 21:41:08 -0400

    Began part 2

--- perftop.c
+++ perftop.c
@@ -7,6 +7,9 @@
 #include <linux/kprobes.h>
 #include <linux/hashtable.h>
 #include <linux/sched.h>
+#include <linux/jhash.h>
+#include <linux/stacktrace.h>
+#define MAX_TRACE 16
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Luke Sanyour");
@@ -16,23 +19,25 @@ static DEFINE_HASHTABLE(pidHashtable, 16);
 //An entry in the hash table
 struct hash_entry {
 
-	int pid_key;
+	u32 hash_key;
 	int numSchedules;
 	struct hlist_node hash_list;
+	unsigned long *stack_trace;
 };
 //function and add and update PIDs in the hash table
-void addPid(int pid)
+void addPid(u32 stackTraceValue, unsigned long *stack_trace)
 {	
 	int bkt;
 	struct hash_entry *current_hash_entry;
 	struct hash_entry *he = kmalloc(sizeof(*he), GFP_ATOMIC);
-
+	//hash the stack trace and PID value
+	u32 hash = jhash(&stackTraceValue, sizeof(stackTraceValue), 0);	
 	//check if PID is in hash table and update
 	if(!hash_empty(pidHashtable))
 	{
 		hash_for_each(pidHashtable, bkt, current_hash_entry, hash_list)
 		{
-			if(current_hash_entry->pid_key == pid)
+			if(current_hash_entry->hash_key == hash)
 			{
 				current_hash_entry->numSchedules++;
 				return;
@@ -42,11 +47,11 @@ void addPid(int pid)
 	//otherwise, add it to the hash table
 	if(he != NULL)
 	{
-		he->pid_key = pid;
+		he->hash_key = hash;
 		he->numSchedules = 1;
-		hash_add(pidHashtable, &he->hash_list, pid);
+		he->stack_trace = stack_trace;
+		hash_add(pidHashtable, &he->hash_list, hash);
 	}
-
 }
 //function to delete the PID hash table
 void deleteHashTable(void)
@@ -61,6 +66,11 @@ void deleteHashTable(void)
 //entry handler for Kretprobe
 static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 {
+	unsigned long *stackTrace = kmalloc(sizeof(*stackTrace), GFP_ATOMIC);
+	u32 stackTraceValue = 0;
+	int i = 0;
+	u32 pid;
+
 	//retrive current task, which is the second argument of pick_next_task_fair,
 	//stored in rsi register by standard x86 64-bit calling convention
 	if(regs->si)
@@ -68,18 +78,34 @@ static int entry_handler(struct kretprobe_instance *ri, struct pt_regs *regs)
 		struct task_struct *p = (struct task_struct *)regs->si;
 		if(p != NULL && p->pid)
 		{	
-			addPid(p->pid);
+			pid = p->pid;
+			stack_trace_save(stackTrace, MAX_TRACE, 0);
+			//convert the stack trace into a usable value
+			for(i = 0; i < MAX_TRACE; i++)
+				stackTraceValue += stackTrace[i];
+
+			stackTraceValue += pid;
+			addPid(stackTraceValue, stackTrace);
 		}
 	}
+
 	return 0;
 }
 //create proc file
 static int perftop_show(struct seq_file *m, void *v) {
 	int bkt;
+	int i = 0;
 	struct hash_entry *current_hash_entry;	
+	unsigned long *current_stack_trace;
 	hash_for_each(pidHashtable, bkt, current_hash_entry, hash_list)
 	{
-		seq_printf(m, "PID: %d      # Of Schedules: %d\n ", current_hash_entry->pid_key, current_hash_entry->numSchedules);
+		current_stack_trace = current_hash_entry->stack_trace;
+		seq_printf(m, "Stack Trace: \n");
+		for(i = 0; i < MAX_TRACE; i++)
+		{
+			seq_printf(m, "%lu\n", current_stack_trace[i]);
+		}	
+		seq_printf(m, "# Of Schedules: %d\n ", current_hash_entry->numSchedules);
 	}
 	return 0;
 
